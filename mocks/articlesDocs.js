8b590052d727763003a9f98257a1f07032947d59
Iryna Murzak <iramurzak@gmail.com>
2023-12-02 17:23:49 +0200

    create mock data

---
+++ mocks/articlesDocs.js
@@ -0,0 +1,52 @@
+const articlesDocs = [
+  {
+    name: "Express - introduction",
+    description: "Express - text",
+    type: "a",
+    tags: [],
+  },
+  {
+    name: "Node.js - introduction",
+    description: "Node.js - text",
+    type: "a",
+    tags: [],
+  },
+  {
+    name: "Angular - introduction",
+    description: "Angular - text",
+    type: "b",
+    tags: [],
+  },
+  {
+    name: "React - introduction",
+    description: "React - text",
+    type: "b",
+    tags: [],
+  },
+  {
+    name: "Vue.js - introduction",
+    description: "Vue.js - text",
+    type: "b",
+    tags: [],
+  },
+  {
+    name: "Javascript - introduction",
+    description: "Javascript - text",
+    type: "b",
+    tags: [],
+  },
+  {
+    name: "CSS - introduction",
+    description: "CSS - text",
+    type: "c",
+    tags: [],
+  },
+  {
+    name: "HTML - introduction",
+    description: "HTML - text",
+    type: "c",
+    tags: [],
+  },
+];
+
+module.exports = { articlesDocs };
---
+++ mocks/studentsStats.js
@@ -0,0 +1,140 @@
+const studentsStats = [
+  {
+    name: "Aimee Zank",
+    scores: [
+      {
+        score: 1.463179736705023,
+        type: "exam",
+      },
+      {
+        score: 11.78273309957772,
+        type: "quiz",
+      },
+      {
+        score: 35.8740349954354,
+        type: "homework",
+      },
+    ],
+  },
+  {
+    name: "John Doe",
+    scores: [
+      {
+        score: 5.98383727783,
+        type: "exam",
+      },
+      {
+        score: 2.736363728292993,
+        type: "quiz",
+      },
+      {
+        score: 10.9398373772,
+        type: "homework",
+      },
+    ],
+  },
+  {
+    name: "Alice Johnson",
+    scores: [
+      {
+        score: 10.8277372722,
+        type: "exam",
+      },
+      {
+        score: 19.88373737782,
+        type: "quiz",
+      },
+      {
+        score: 37.848484994949,
+        type: "homework",
+      },
+    ],
+  },
+  {
+    name: "Jake Williams",
+    scores: [
+      {
+        score: 35.8277372722,
+        type: "exam",
+      },
+      {
+        score: 12.88574773733,
+        type: "quiz",
+      },
+      {
+        score: 5.393827227728,
+        type: "homework",
+      },
+    ],
+  },
+  {
+    name: "Ann Brown",
+    scores: [
+      {
+        score: 55.488483839393,
+        type: "exam",
+      },
+      {
+        score: 15.88838383982,
+        type: "quiz",
+      },
+      {
+        score: 91.828282773,
+        type: "homework",
+      },
+    ],
+  },
+  {
+    name: "John Swift",
+    scores: [
+      {
+        score: 61.9933883772,
+        type: "exam",
+      },
+      {
+        score: 46.9933838882,
+        type: "quiz",
+      },
+      {
+        score: 93.99383772782,
+        type: "homework",
+      },
+    ],
+  },
+  {
+    name: "Alice Miller",
+    scores: [
+      {
+        score: 33.887266663,
+        type: "exam",
+      },
+      {
+        score: 12.009872666,
+        type: "quiz",
+      },
+      {
+        score: 82.865525636,
+        type: "homework",
+      },
+    ],
+  },
+  {
+    name: "Alex Backer",
+    scores: [
+      {
+        score: 55.772782929,
+        type: "exam",
+      },
+      {
+        score: 62.983763662,
+        type: "quiz",
+      },
+      {
+        score: 39.762662662,
+        type: "homework",
+      },
+    ],
+  },
+];
+
+module.exports = { studentsStats };
