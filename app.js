52ca2ef2264b4c55472ddfcdac0cd72160dd9c7d
Iryna Murzak <iramurzak@gmail.com>
2023-12-03 19:17:53 +0200

    add app-level request logger

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 const port = 3000;
 const indexRouter = require("./routes/index");
 
+app.use((req, res, next) => {
+  const timestamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.url;
+  console.log(`${timestamp} - ${method} ${url}`);
+  next();
+});
+
 app.use(express.json());
 app.use("/", indexRouter);
 
