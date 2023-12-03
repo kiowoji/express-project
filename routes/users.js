291d30f99dbce1d120a2b3429a6e58586a2cf878
Iryna Murzak <iramurzak@gmail.com>
2023-12-03 19:42:46 +0200

    add router-level mdware

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,16 @@ const usersController = require("../controllers/users.controller");
 const { userExists, validateUserPayload } = usersController.middleware;
 const { validateBody } = require("../middlewares/users.mdware");
 
-router.get("/", usersController.getUsers);
-router.post("/", validateBody, validateUserPayload, usersController.addUser);
+const logRequestDetails = (req, res, next) => {
+  console.log(`Request Body: ${JSON.stringify(req.body)}`);
+  console.log(`Request Params: ${JSON.stringify(req.params)}`);
+  next();
+};
+
+router.get("/", logRequestDetails, usersController.getUsers);
+router.post("/", logRequestDetails, validateBody, validateUserPayload, usersController.addUser);
 router.get("/:email", userExists, usersController.getUserByEmail);
-router.put(
-  "/:email",
-  validateBody,
-  userExists,
-  validateUserPayload,
-  usersController.updateUser
-);
+router.put("/:email", logRequestDetails, validateBody, userExists, validateUserPayload, usersController.updateUser);
 router.delete("/:email", userExists, usersController.deleteUser);
 
 module.exports = router;
